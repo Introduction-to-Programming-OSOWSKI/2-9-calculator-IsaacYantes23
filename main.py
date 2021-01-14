b2bd7539d82579baf8588f2c2f59fc5e564b069c
IsaacYantes23 <817296@mystma.org>
2021-01-14 18:02:57 +0000

    calculator try 2

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-def calculate(x,y):
-    if "add":
+def calculate(option,x,y):
+    if option == "add":
         return x + y
-    elif "subtract":
+    elif option == "subtract":
         return x - y
-    elif "multiply":
+    elif option == "multiply":
         return x * y
     else:
         return x / y
 
-print(calculate("subtract"))
-print(calculate(10,10))
+print(calculate("multiply",10, 10))
