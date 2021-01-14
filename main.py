8b7065f38dba8e6258cbdd468e90ce2005e8ec19
IsaacYantes23 <817296@mystma.org>
2021-01-14 16:16:50 +0000

    calculator try 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculate(x,y):
+    if "add":
+        return x + y
+    elif "subtract":
+        return x - y
+    elif "multiply":
+        return x * y
+    else:
+        return x / y
+
+print(calculate("subtract"))
+print(calculate(10,10))
